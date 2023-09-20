e17f7bb1d596a2e247a72b38dc5ebd9ed94c5132
sighakalaurent <nguifosighaka@gmail.com>
2023-09-20 16:26:40 +0200

    exercice1

---
+++ exercice1.js
@@ -0,0 +1,12 @@
+/* <script>
+function somme(nbr1, nbr2,nbr3,sum) {
+
+    nbr1 = Number(document.getElementById("nbr1").value)
+    nbr2 = Number(document.getElementById("nbr2").value)
+    nbr3 = Number(document.getElementById("nbr3").value)
+    sum= nbr1 + nbr2 + nbr3;
+    document.getElementById("sum").value = sum;
+
+}
+
+</script> */
